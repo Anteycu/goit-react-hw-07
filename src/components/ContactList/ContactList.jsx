f2db176acaa2bbdd5dcec4557e8b62ead4fec589
Mykola T <anteycu@gmail.com>
2024-10-15 18:26:36 +0300

    implemented asyncThunk

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,8 +1,7 @@
 import { useSelector } from "react-redux";
-import { selectContacts } from "../../redux/contactsSlice";
-import { selectNameFilter } from "../../redux/filtersSlice";
 import Contact from "../Contact/Contact";
 import css from "./ContactList.module.css";
+import { selectContacts, selectNameFilter } from "../../redux/selectors";
 
 const ContactList = () => {
   const contacts = useSelector(selectContacts);
