3983978ff985a90ea6bf5d8a53b840c292d739e4
Mykola T <anteycu@gmail.com>
2024-10-17 16:14:09 +0300

    implemented memoization of visibleContacts selector

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,19 +1,14 @@
 import { useSelector } from "react-redux";
 import Contact from "../Contact/Contact";
 import css from "./ContactList.module.css";
-import { selectContacts, selectNameFilter } from "../../redux/selectors";
+import { selectMemoVisibleContacts } from "../../redux/selectors";
 
 const ContactList = () => {
-  const contacts = useSelector(selectContacts);
-  const searchedName = useSelector(selectNameFilter);
-
-  const visibleContacts = contacts.filter((item) =>
-    item.name.toLowerCase().includes(searchedName.toLowerCase())
-  );
+  const contacts = useSelector(selectMemoVisibleContacts);
 
   return (
     <ul className={css.contacts}>
-      {visibleContacts.map(({ id, name, number }) => (
+      {contacts.map(({ id, name, number }) => (
         <li className={css.contactItem} key={id}>
           <Contact username={name} phoneNum={number} contactId={id} />
         </li>
